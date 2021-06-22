9cd3c718b95698c16be0180f065cb684dd1598c7
vinuri-s <piyathilakev@gmail.com>
2021-06-23 00:45:43 +0530

    initial commit

---
+++ src/thogakade/ThogaKade.java
@@ -0,0 +1,12 @@
+package thogakade;
+
+public class ThogaKade {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
